8ed2345011b157c3a66be7c060857f1d300ae499
Amar Kaygude <amarkaygude09@gmail.com>
2026-04-28 18:44:40 +0530

    fixed bug in swagger

--- src/Swagger.js
+++ src/Swagger.js
@@ -5,11 +5,11 @@ const doc = {
     title: "Labour Management API",
     description: "API for Labour Hiring Platform",
   },
-  host: "localhost:3000", // match your server port
-  schemes: ["http"],
+  host: "labour-management-backend.onrender.com", // production host
+  schemes: ["https"], // use https for Render
 };
 
-const outputFile = "./swagger.json"; // ✅ match your app.js
-const endpointsFiles = ["./app.js"]; // ✅ correct entry
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./app.js"];
 
 swaggerAutogen()(outputFile, endpointsFiles, doc);
